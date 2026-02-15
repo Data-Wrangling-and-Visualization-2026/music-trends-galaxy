4dcfa7092cc48ae58a0c9db612275580d68f594a
CaneRig <originalomail@gmail.com>
2026-02-15 06:12:55 +0300

    add: blueprint of the stage

---
+++ data_pipeline/stages/01_lyric_fetch/main.py
@@ -0,0 +1,7 @@
+from pipeman import DataPipelineContext
+
+def main(cxt: DataPipelineContext) -> None:
+    ...
+
+def desc() -> str:
+    return '''Fetch lyrics for the songs from input.csv file'''
