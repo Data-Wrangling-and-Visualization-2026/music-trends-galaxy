38edc21ea02c43245174c893f9c8db008382551c
CaneRig <originalomail@gmail.com>
2026-03-16 23:46:50 +0300

    fix: refactor

--- data_pipeline/stages/01_lyric_fetch/main.py
+++ data_pipeline/stages/01_lyric_fetch/main.py
@@ -1,7 +1,22 @@
-from pipeman import DataPipelineContext
+from parsers import *
+
+
+class DataPipelineContext:
+    pass
+
 
 def main(cxt: DataPipelineContext) -> None:
-    ...
+    a = ChainParser(parser_types=get_all_parsers())
+
+    b = a.get_lyrics(track_name='alligator blood', artist_name='')
+    # b = a.search(track_name=input('track: '), artist_name=input('artist: '))
+    # print(b or 'Error')
+    print(b.lyrics)
+    print(b.lyrics_source)
 
 def desc() -> str:
-    return '''Fetch lyrics for the songs from input.csv file'''
+    return "Fetch lyrics for the songs from input.csv file"
+
+
+# if __name__ == 'main':
+main(None)
